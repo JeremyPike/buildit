2717c2318a248eed3fc1dffbfa6709f03a215478
pikeja gw4ops-i3 <pikeja.gw4ops-i3@login01.head.cm.i3.isambard.ac.uk>
2025-06-23 14:51:18 +0000

    benchio package

---
+++ repo/v0.23/isamrepo/packages/benchio/package.py
@@ -0,0 +1,55 @@
+
+from spack.package import * 
+import os
+import subprocess
+
+class Benchio(Package):
+    """BenchIO: A benchmark I/O tool (example description)"""
+
+    phases = ['build', 'install']
+
+    homepage = "https://github.com/davidhenty/benchio"
+    git = "https://github.com/JeremyPike/benchio.git"
+
+    #Track the isambard3 branch
+    version("isambard3", branch="isambard3")
+
+    depends_on('mpi')
+    depends_on("hdf5+mpi+fortran")
+    depends_on("netcdf-fortran")
+    depends_on("adios2")
+
+    @property
+    def build_targets(self):
+
+        targetlist = []
+        targetlist.append("COMMS_ARCH=mpi")
+        targetlist.append(f"FC={self.spec['mpi'].mpifc}")
+
+        adios2_fortran_flags = subprocess.getoutput("adios2-config --fortran-flags")
+        adios2_fortran_libs = subprocess.getoutput("adios2-config --fortran-libs")
+        # Construct FFLAGS
+        fflags = (
+                "-O3 "
+                "-mcpu=neoverse-v2 "
+                f"{adios2_fortran_flags}"
+                )
+
+        # Construct LFLAGS
+        lflags = (
+                "-lnetcdff -lnetcdf -lhdf5_fortran -lhdf5 "
+                f"{adios2_fortran_libs}"
+                )
+
+        targetlist.append(f"FFLAGS={fflags}")
+        targetlist.append(f"LFLAGS={lflags}")
+
+        return targetlist
+
+    def build(self, spec, prefix):
+        make(*self.build_targets)
+
+    def install(self, spec, prefix):
+        # No install needed, just copy the executable to prefix.bin
+        mkdirp(prefix.bin)
+        install('benchio', prefix.bin)
