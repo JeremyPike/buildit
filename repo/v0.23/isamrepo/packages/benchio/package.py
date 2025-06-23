60ad92140e5adcdd5135a4002936a1702b51b9b3
JeremyPike <jeremypike2@gmail.com>
2025-06-23 16:31:41 +0100

    linting

--- repo/v0.23/isamrepo/packages/benchio/package.py
+++ repo/v0.23/isamrepo/packages/benchio/package.py
@@ -1,19 +1,18 @@
 
 from spack.package import * 
-import os
 import subprocess
 
 class Benchio(Package):
-    """BenchIO: A benchmark I/O tool (example description)"""
+    """BenchIO: A benchmark I/O tool"""
 
-    phases = ['build', 'install']
+    phases = ["build", "install"]
 
     homepage = "https://github.com/davidhenty/benchio"
     git = "https://github.com/davidhenty/benchio.git"
 
     version("master", branch="master")
 
-    depends_on('mpi')
+    depends_on("mpi")
     depends_on("hdf5+mpi+fortran")
     depends_on("netcdf-fortran")
     depends_on("adios2")
@@ -25,18 +24,15 @@ class Benchio(Package):
         targetlist.append("COMMS_ARCH=mpi")
         targetlist.append(f"FC={self.spec['mpi'].mpifc}")
 
-        adios2_fortran_flags = subprocess.getoutput("adios2-config --fortran-flags")
-        adios2_fortran_libs = subprocess.getoutput("adios2-config --fortran-libs")
-
         fflags = (
                 "-O3 "
                 "-mcpu=neoverse-v2 "
-                f"{adios2_fortran_flags}"
+                f"{subprocess.getoutput('adios2-config --fortran-flags')}"
                 )
 
         lflags = (
                 "-lnetcdff -lnetcdf -lhdf5_fortran -lhdf5 "
-                f"{adios2_fortran_libs}"
+                f"{subprocess.getoutput('adios2-config --fortran-libs')}"
                 )
 
         targetlist.append(f"FFLAGS={fflags}")
@@ -45,9 +41,9 @@ class Benchio(Package):
         return targetlist
 
     def build(self, spec, prefix):
-        make('-f', 'Makefile-archer2', *self.build_targets)
+        make("-f", "Makefile-archer2", *self.build_targets)
 
     def install(self, spec, prefix):
         # No install needed, just copy the executable to prefix.bin
         mkdirp(prefix.bin)
-        install('benchio', prefix.bin)
+        install("benchio", prefix.bin)
